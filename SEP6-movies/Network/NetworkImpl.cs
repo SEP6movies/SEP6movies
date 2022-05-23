7384c3acb9b49b0f1d39af6f61af0405faf3627b
martinb264 <58074983+martinb264@users.noreply.github.com>
2022-05-23 15:18:28 +0200

    More calls with api and db

--- SEP6-movies/Network/NetworkImpl.cs
+++ SEP6-movies/Network/NetworkImpl.cs
@@ -37,7 +37,14 @@ namespace test_shit.Network
             Console.WriteLine(_movie.original_title);
             Console.WriteLine(actor.name);
             Console.WriteLine(credits.cast.Count);
+            
+
+            TopRated movies = new TopRated();
+            movies = await getTopRatedMoviesFromApi();
+            
+            Console.WriteLine(movies.results[0].original_title);
             */
+            
         }
 
 
@@ -139,19 +146,18 @@ namespace test_shit.Network
                 
 
                 command = new SqlCommand(sql, rds);
-                command.Parameters.AddWithValue("@date", DateTime.Now.ToString("yyyy-MM-dd"));
-                command.Parameters.AddWithValue("@kage", "test1223");
+                
 
-                command.ExecuteNonQuery();
+                dataReader = command.ExecuteReader();
 
 
-                /*
+                
                 while (dataReader.Read())
                 {
-                 
-                }*/
+                 movieIds.Add(dataReader.GetInt32(0));
+                }
         
-                //dataReader.Close();
+                dataReader.Close();
 
                 rds.Close();
 
@@ -165,9 +171,104 @@ namespace test_shit.Network
             return movieIds;
         }
 
-        public void apiTest()
+        public List<int> getFavorites(int userID)
         {
+            List<int> movieFavoritesIDs = new List<int>();
+            try
+            {
+               
+                rds = new SqlConnection(connectionString);
+              
+                rds.Open();
+
+                string sql = "select MovieId from dbo.Favorites where UserId=@user";
+                
+
+                command = new SqlCommand(sql, rds);
+                command.Parameters.AddWithValue("@user",userID);
+                
+
+                dataReader = command.ExecuteReader();
+
+
+                
+                while (dataReader.Read())
+                {
+                    movieFavoritesIDs.Add(dataReader.GetInt32(0));
+                }
+        
+                dataReader.Close();
+
+                rds.Close();
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.StackTrace);
+            }
+
+
+
+            return movieFavoritesIDs;
+        }
+
+        public async Task<PopularActors> getPopularActors()
+        {
+            PopularActors popularActors = new PopularActors();
+            HttpResponseMessage responseMessage = await client.GetAsync(
+                "https://api.themoviedb.org/3/person/popular?api_key=088cf42d74dfbb21a6c0d01269bd904a&language=en-US&page=1");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+               popularActors = await responseMessage.Content.ReadAsAsync<PopularActors>();
+            }
+
+            return popularActors;
+        }
+
+        public void addMovieToFavorites(int userId,int movieId)
+        {
+            try
+            {
+
+                rds = new SqlConnection(connectionString);
+
+                rds.Open();
+
+                string sql = "insert into dbo.Favorites values(@user,@movie)";
+
+
+                command = new SqlCommand(sql, rds);
+                command.Parameters.AddWithValue("@user", userId);
+                command.Parameters.AddWithValue("@movie", movieId);
+
+
+                command.ExecuteNonQuery();
+
+
+
+
+                rds.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.StackTrace);
+            }
+        }
+
+        public async Task<TopRated> getTopRatedMoviesFromApi()
+        {
+            TopRated movies = new TopRated();
             
+            HttpResponseMessage responseMessage = await client.GetAsync(
+                "https://api.themoviedb.org/3/movie/top_rated?api_key=088cf42d74dfbb21a6c0d01269bd904a&language=en-US&page=1");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                movies = await responseMessage.Content.ReadAsAsync<TopRated>();
+                
+            }
+           
+            return movies;
         }
     }
 }

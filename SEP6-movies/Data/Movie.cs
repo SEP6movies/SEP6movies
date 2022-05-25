e4c8863e2d743a58f176bcbdf13bd957e71c34bf
martinb264 <58074983+martinb264@users.noreply.github.com>
2022-05-25 14:57:03 +0200

    Fix to Movie class allowing null

--- SEP6-movies/Data/Movie.cs
+++ SEP6-movies/Data/Movie.cs
@@ -7,7 +7,7 @@ namespace test_shit.Data
         public bool adult { get; set; }
         public string backdrop_path { get; set; }
         public BelongsToCollection belongs_to_collection { get; set; }
-        public int budget { get; set; }
+        public int? budget { get; set; }
         public List<Genre> genres { get; set; }
         public string homepage { get; set; }
         public int id { get; set; }
@@ -20,15 +20,15 @@ namespace test_shit.Data
         public List<ProductionCompany> production_companies { get; set; }
         public List<ProductionCountry> production_countries { get; set; }
         public string release_date { get; set; }
-        public int revenue { get; set; }
-        public int runtime { get; set; }
+        public int? revenue { get; set; }
+        public int? runtime { get; set; }
         public List<SpokenLanguage> spoken_languages { get; set; }
         public string status { get; set; }
         public string tagline { get; set; }
         public string title { get; set; }
         public bool video { get; set; }
         public double vote_average { get; set; }
-        public int vote_count { get; set; }
+        public int? vote_count { get; set; }
 
         public class BelongsToCollection
         {
